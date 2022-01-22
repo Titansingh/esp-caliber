d421ca4997575d195870f13dfbf9c7a0eb9f815e
Anurag-Kumar-Mishra <akm4093@gmail.com>
2022-01-22 22:12:36 +0530

    Removed "jdk7" suffix from kotlin stdlib dependency to remove jdk inconsistency related problems

--- app/src/main/java/com/example/ebook/rc_main_adapter.kt
+++ app/src/main/java/com/example/ebook/rc_main_adapter.kt
@@ -7,6 +7,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import java.util.*
 
 class rc_main_adapter(private val bookList: ArrayList<Book>) : RecyclerView.Adapter<rc_main_adapter.MyViewHolder>() {
     override fun onCreateViewHolder(
